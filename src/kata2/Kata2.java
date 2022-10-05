c13837882ee194f8b67829132cc5cfe925fb4783
aagir <aagir@DESKTOP-9I5659K>
2022-10-05 09:35:55 +0100

    version 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,11 +5,11 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer[] data = {4, 1, 2, 4, 7, 9, 3, 5, 7, 2, 0, 1, 3, 5, 3, 3, 5};
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         HistogramGenerator generator = new HistogramGenerator(data);
-        Map<Integer, Integer> histogram = generator.getHistogram();
+        Map<String, Integer> histogram = generator.getHistogram();
         
-        for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+        for(Map.Entry<String, Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + " ===> " + entry.getValue());
         }
     }
