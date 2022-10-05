00b2cb12259b6acde2df8fa9d73980edb5ae3c63
aagir <aagir@DESKTOP-9I5659K>
2022-10-05 09:19:37 +0100

    version 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,13 +9,9 @@ public class Kata2 {
         int[] data = {4, 1, 2, 4, 7, 9, 3, 5, 7, 2, 0, 1, 3, 5, 3, 3, 5};
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            }
-            else {
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? 
+                            histogram.get(key) + 1 : 1);
         }
         for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + " ===> " + entry.getValue());
